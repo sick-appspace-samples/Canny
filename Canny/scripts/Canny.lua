666b6f9405fea049a59384e8c15697364e46638b
rdzanmi <mikolaj.rdzanek@sick.de>
2020-12-16 12:10:59 +0100

    'Update'

--- Canny/scripts/Canny.lua
+++ Canny/scripts/Canny.lua
@@ -27,7 +27,7 @@ print('AppEngine Version: ' .. Engine.getVersion())
 local DELAY = 700 -- ms between visualization steps for demonstration purpose
 
 -- Creating viewer
-local viewer = View.create()
+local viewer = View.create("viewer2D1")
 
 -- Setting up graphical overlay attributes
 local decoration = View.ShapeDecoration.create()
